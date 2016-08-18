881cc9a4ab0e49973bb04e5f77a6c773148ce6db
Anderson Luiz Mendes Matos <anderson.matos@nacionalsoft.com.br>
2016-08-18 14:05:16 -0300

    Fixed project.json properties, generated initial pack script file for extensions and last commit before stable version.

---
+++ nuget/DataTables.AspNet.Extensions.DapperExtensions.pack.bat
@@ -0,0 +1,3 @@
+cd ..\src\DataTables.AspNet.Extensions.DapperExtensions
+dotnet pack -b ..\..\artifacts\nuget-obj\DataTables.AspNet.Extensions.DapperExtensions\ -o ..\..\artifacts\nuget\ -c Release
+cd ..\..\nuget
