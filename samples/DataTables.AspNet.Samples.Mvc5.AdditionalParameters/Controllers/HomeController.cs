b3179a67e8281888e134af7b279b167299667f40
Anderson Luiz Mendes Matos <anderson@andersonmatos.com.br>
2017-03-08 08:48:18 -0300

    Created sample project to demonstrate the use of additional parameters (MVC5). Upgraded to .NET Tools 1.0 and VS 2017.

---
+++ samples/DataTables.AspNet.Samples.Mvc5.AdditionalParameters/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿#region Copyright
+/* The MIT License (MIT)
+
+Copyright (c) 2014 Anderson Luiz Mendes Matos (Brazil)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#endregion Copyright
+
+using System.Linq;
+using System.Web.Mvc;
+using DataTables.AspNet.Core;
+using DataTables.AspNet.Mvc5;
+using System.Collections.Generic;
+
+namespace DataTables.AspNet.Samples.Mvc5.BasicIntegration.Controllers
+{
+    public class HomeController : Controller
+    {
+        // This is your basic view method.
+        // Nothing is done here besides view returning.
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// This is your data method.
+        /// DataTables will query this (HTTP GET) to fetch data to display.
+        /// </summary>
+        /// <param name="request">
+        /// This represents your DataTables request.
+        /// It's automatically binded using the default binder and settings.
+        /// 
+        /// You should use IDataTablesRequest as your model, to avoid unexpected behavior and allow
+        /// custom binders to be attached whenever necessary.
+        /// </param>
+        /// <returns>
+        /// Return data here, with a json-compatible result.
+        /// </returns>
+        public ActionResult PageData(IDataTablesRequest request)
+        {
+            // Nothing important here. Just creates some mock data.
+            var data = Models.SampleEntity.GetSampleData();
+            
+            // Check additional parameters. You may perform custom actions with them.
+            foreach(var additionalParameter in request.AdditionalParameters)
+            {
+                System.Diagnostics.Debug.WriteLine(additionalParameter.Key + ": " + additionalParameter.Value);
+            }
+
+            // Global filtering.
+            // Filter is being manually applied due to in-memmory (IEnumerable) data.
+            // If you want something rather easier, check IEnumerableExtensions Sample.
+            var filteredData = data.Where(_item => _item.Name.Contains(request.Search.Value));
+
+            // Paging filtered data.
+            // Paging is rather manual due to in-memmory (IEnumerable) data.
+            var dataPage = filteredData.Skip(request.Start).Take(request.Length);
+
+            // Create some response additional parameters.
+            var returnParameters = new Dictionary<string, object>()
+            {
+                { "rParam1", "First parameter" },
+                { "rParam2", 2017 }
+            };
+
+            // Response creation. To create your response you need to reference your request, to avoid
+            // request/response tampering and to ensure response will be correctly created.
+            var response = DataTablesResponse.Create(request, data.Count(), filteredData.Count(), dataPage, returnParameters);
+
+            // Easier way is to return a new 'DataTablesJsonResult', which will automatically convert your
+            // response to a json-compatible content, so DataTables can read it when received.
+            return new DataTablesJsonResult(response, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ samples/DataTables.AspNet.Samples.Mvc5.AdditionalParameters/Global.asax.cs
@@ -0,0 +1,63 @@
+﻿#region Copyright
+/* The MIT License (MIT)
+
+Copyright (c) 2014 Anderson Luiz Mendes Matos (Brazil)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#endregion Copyright
+
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace DataTables.AspNet.Samples.Mvc5.BasicIntegration
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            // DataTables.AspNet registration with default options.
+            var options = new DataTables.AspNet.Mvc5.Options()
+                .EnableRequestAdditionalParameters()
+                .EnableResponseAdditionalParameters();
+
+            var binder = new DataTables.AspNet.Mvc5.ModelBinder();
+            binder.ParseAdditionalParameters = Parser;
+
+            DataTables.AspNet.Mvc5.Configuration.RegisterDataTables(options, binder);
+        }
+
+
+        private IDictionary<string, object> Parser(ControllerContext controllerContext, ModelBindingContext modelBindingContext)
+        {
+            var p1 = System.Convert.ToString(modelBindingContext.ValueProvider.GetValue("param1").AttemptedValue);
+            var p2 = System.Convert.ToInt32(modelBindingContext.ValueProvider.GetValue("param2").AttemptedValue);
+
+            return new Dictionary<string, object>()
+            {
+                { "param1", p1 },
+                { "param2", p2 }
+            };
+        }
+    }
+}
